eecabc3b3c08a805c6221db38f6dc14bbc87fbab
dannyfrelink <danny-frelink@hotmail.com>
2022-01-11 17:25:41 +0100

    test animeren op een laag van SVG

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ gsap.to('.square', {
 	scrollTrigger: {
 		trigger: '.square',
 		scrub: true,
-		start: 'top center ',
-		end: 'top 100px',
+		start: 'top 80%',
+		end: 'top 20%',
 		markers: true,
 	},
 	x: 600,
 	rotation: 360,
 	duration: 3,
+})
+
+gsap.to('.test2', {
+	scrollTrigger: {
+		trigger: '.test2',
+		scrub: true,
+		start: 'top 80%',
+		end: 'top 20%',
+		markers: true,
+	},
+	x: 200,
+	rotation: 270,
+	duration: 5
 })
