9e42e5c4fee4b05746f448e23e983dcd94fd2b1e
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-01-13 17:14:34 +0100

    test

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-gsap.to('.square', {
-	scrollTrigger: {
-		trigger: '.square',
-		scrub: true,
-		start: 'top center ',
-		end: 'top 100px',
-		markers: true,
-	},
-	x: 600,
-	rotation: 360,
-	duration: 3,
-})
+
