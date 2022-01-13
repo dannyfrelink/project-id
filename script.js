462a13afdcf3fd33184490932a6db33e1cc7829d
dannyfrelink <danny-frelink@hotmail.com>
2022-01-13 14:30:49 +0100

    hype pagina + animatie avocado naar insta post

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-// gsap.to('.square', {
-// 	scrollTrigger: {
-// 		trigger: '.square',
-// 		scrub: true,
-// 		start: 'top 80%',
-// 		end: 'top 20%',
-// 		markers: true,
-// 	},
-// 	x: 600,
-// 	rotation: 360,
-// 	duration: 3,
-// })
-
-// gsap.to('.test2', {
-// 	scrollTrigger: {
-// 		trigger: '.test2',
-// 		scrub: true,
-// 		start: 'top 80%',
-// 		end: 'top 20%',
-// 		markers: true,
-// 	},
-// 	x: 200,
-// 	rotation: 270,
-// 	duration: 5
-// })
+gsap.to('.avocado', {
+	scrollTrigger: {
+		trigger: '.avocado',
+		scrub: true,
+		start: 'top 60%',
+		end: 'center top',
+		// markers: true,
+	},
+	y: '25vw',
+	rotation: 360,
+	duration: 13,
+})
