9c3773187b73a0e9d7bb925ec7507d29045b831c
dannyfrelink <danny-frelink@hotmail.com>
2022-01-13 13:19:04 +0100

    compleet maken van de header pagina

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-gsap.to('.square', {
-	scrollTrigger: {
-		trigger: '.square',
-		scrub: true,
-		start: 'top 80%',
-		end: 'top 20%',
-		markers: true,
-	},
-	x: 600,
-	rotation: 360,
-	duration: 3,
-})
+// gsap.to('.square', {
+// 	scrollTrigger: {
+// 		trigger: '.square',
+// 		scrub: true,
+// 		start: 'top 80%',
+// 		end: 'top 20%',
+// 		markers: true,
+// 	},
+// 	x: 600,
+// 	rotation: 360,
+// 	duration: 3,
+// })
 
-gsap.to('.test2', {
-	scrollTrigger: {
-		trigger: '.test2',
-		scrub: true,
-		start: 'top 80%',
-		end: 'top 20%',
-		markers: true,
-	},
-	x: 200,
-	rotation: 270,
-	duration: 5
-})
+// gsap.to('.test2', {
+// 	scrollTrigger: {
+// 		trigger: '.test2',
+// 		scrub: true,
+// 		start: 'top 80%',
+// 		end: 'top 20%',
+// 		markers: true,
+// 	},
+// 	x: 200,
+// 	rotation: 270,
+// 	duration: 5
+// })
