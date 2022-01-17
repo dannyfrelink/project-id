9b3348a520c3edc8f07b2a179877b1df3ab449a7
dannyfrelink <danny-frelink@hotmail.com>
2022-01-17 13:23:26 +0100

    eerste deel pinning en animeren 4-pollution

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ tl
 				trigger: '.avocado',
 				start: '200% 39%',
 				end: '200% top',
-				markers: true,
 				scrub: 0.75
 			},
 			rotation: 360,
@@ -24,13 +23,46 @@ tl
 				trigger: '.avocado',
 				start: 'top 70%',
 				end: 'top 40%',
-				markers: true,
 				scrub: 0.75
 			},
 			y: '25vw',
 			rotation: 360,
 			duration: 13
 		})
+// .to('.avocado',
+// 	{
+// 		scrollTrigger: {
+// 			trigger: '.avocado',
+// 			start: '500% 70%',
+// 			end: '500% 40%',
+// 			markers: true,
+// 			scrub: 0.75
+// 		},
+// 		rotation: 0,
+// 		opacity: 0,
+// 		transform: 'scale(0.5)',
+// 		duration: 13
+// 	})
+
+// .fromTo('.avocado', {
+// 	x: '-2vw',
+// 	y: '90vw',
+// 	transform: 'scale(1.5)'
+// },
+// 	{
+// 		scrollTrigger: {
+// 			trigger: '.avocado',
+// 			start: '500% 70%',
+// 			end: '500% 40%',
+// 			markers: true,
+// 			scrub: 0.75
+// 		},
+// 		y: '90vw',
+// 		x: '-2vw',
+// 		opacity: 0,
+// 		transform: 'scale(0.5)',
+// 		duration: 13
+// 	})
 
 gsap.from('#Hashtag_1', {
 	scrollTrigger: {
@@ -65,7 +97,41 @@ gsap.from('#Groep_arrow', {
 	stagger: 0.5
 })
 
+// Avocado eten (keerzijde pagina)
+var tl2 = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.keerzijde',
+		pin: '.keerzijde',
+		scrub: 0.75,
+		start: 'top top',
+		end: 'bottom top',
+		markers: true
+	},
+})
 
+tl2.to('#vork', {
+	rotation: -50,
+	x: '2.5vw',
+	y: '5.5vw',
+	transformOrigin: '0% 100%'
+})
+
+tl2.to('#vork, .avocado_bord', {
+	y: '-10vw',
+	duration: 13,
+})
+
+// gsap.to('.avocado_bord', {
+// 	scrollTrigger: {
+// 		trigger: '.avocado_bord',
+// 		scrub: 1,
+// 		// start: 'top bottom',
+// 		end: 'top 80%',
+// 		markers: true
+// 	},
+// 	opacity: 0.5,
+// 	duration: 1
+// })
 
 gsap.to('#De_avocado_tekst', {
 	scrollTrigger: {
@@ -73,22 +139,4 @@ gsap.to('#De_avocado_tekst', {
 		scrub: true,
 	},
 	opacity: '100%',
-	// ease: Expo.easeOut,
 })
-
-// gsap.to('.avocadoFoto', {
-// 	scrollTrigger: {
-// 		trigger: '.avocadoFoto',
-// 		scrub: true,
-// 	},
-// 	x: '0',
-// 	rotate: 720,
-// })
-
-// gsap.to('.titel', {
-// 	scrollTrigger: {
-// 		trigger: '.titel',
-// 		scrub: true,
-// 	},
-// 	x: '0',
-// })
