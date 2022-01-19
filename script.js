d7277f14d1e8927885d46608f35f46b1766296b5
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-01-19 21:09:15 +0100

    bug gefixt (grafiek)

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ gsap.from('#ondertitel', {
 		trigger: '#titel',
 		start: 'top 20%',
 		end: 'top top',
-		scrub: 0.3
+		scrub: 0.3,
 	},
-	opacity: 0
+	opacity: 0,
 })
 
 // Avocado animatie door schermen
@@ -14,39 +14,37 @@ gsap.timeline({
 		trigger: '#avocado',
 		start: 'top 70%',
 		end: 'top 40%',
-		scrub: 0.75
-	}
+		scrub: 0.75,
+	},
+}).to('#avocado', {
+	y: '25vw',
+	rotation: 360,
+	duration: 13,
 })
-	.to('#avocado', {
-		y: '25vw',
-		rotation: 360,
-		duration: 13
-	})
 
 gsap.timeline({
 	scrollTrigger: {
 		trigger: '#avocado',
 		start: '200% 40%',
 		end: '200% top',
-		scrub: 0.75
+		scrub: 0.75,
 	},
+}).to('#avocado', {
+	rotation: 360,
+	x: '-2vw',
+	y: '90vw',
+	transform: 'scale(1.5)',
+	duration: 13,
 })
-	.to('#avocado', {
-		rotation: 360,
-		x: '-2vw',
-		y: '90vw',
-		transform: 'scale(1.5)',
-		duration: 13
-	})
 
 gsap.to('#avocado', {
 	scrollTrigger: {
 		trigger: '#avocado',
 		start: '450% 40%',
 		end: '450% 25%',
-		scrub: 0.75
+		scrub: 0.75,
 	},
-	opacity: 0
+	opacity: 0,
 })
 
 // Hype pagina
@@ -54,20 +52,20 @@ gsap.to('#De_avocado_tekst', {
 	scrollTrigger: {
 		trigger: '#De_avocado_tekst',
 		end: 'top center',
-		scrub: 0.2
+		scrub: 0.2,
 	},
-	opacity: '100%'
+	opacity: '100%',
 })
 
 gsap.from('#Hashtag', {
 	scrollTrigger: {
 		trigger: '#Hashtag',
 		scrub: 1,
-		end: 'top 70%'
+		end: 'top 70%',
 	},
 	x: 400,
 	duration: 1,
-	stagger: 0.5
+	stagger: 0.5,
 })
 
 // Health pagina
@@ -75,22 +73,22 @@ gsap.from('#Groep_text', {
 	scrollTrigger: {
 		trigger: '#Groep_text',
 		scrub: 1,
-		end: 'top 70%'
+		end: 'top 70%',
 	},
 	x: -400,
 	duration: 1,
-	stagger: 0.5
+	stagger: 0.5,
 })
 
 gsap.from('#Groep_arrow', {
 	scrollTrigger: {
 		trigger: '#Groep_arrow',
 		scrub: 1,
-		end: 'top 70%'
+		end: 'top 70%',
 	},
 	transform: 'scale(0)',
 	duration: 1,
-	stagger: 0.5
+	stagger: 0.5,
 })
 
 // Avocado eten (keerzijde pagina)
@@ -101,27 +99,27 @@ gsap.timeline({
 		scrub: 0.5,
 		start: 'top top',
 		end: '+=100%',
-		pinSpacing: true
+		pinSpacing: true,
 	},
 })
 	.to('#vork', {
 		rotation: -50,
 		x: '2.5vw',
 		y: '5.5vw',
-		transformOrigin: '0% 100%'
+		transformOrigin: '0% 100%',
 	})
 	.to('#vork_avocado, #hand', {
 		y: '-14vw',
-		duration: 13
+		duration: 13,
 	})
 	.from('#stopbord', {
 		x: 700,
-		duration: 13
+		duration: 13,
 	})
 	.to('#avocado_eten', {
 		y: 2000,
 		duration: 30,
-		rotation: 90
+		rotation: 90,
 	})
 
 // Chili pagina
@@ -130,26 +128,26 @@ gsap.from('#petorca_text>*', {
 		trigger: '#petorca_text',
 		scrub: 0.75,
 		start: 'top 80%',
-		end: 'top 35%'
+		end: 'top 35%',
 	},
 	opacity: 0,
-	stagger: 0.5
+	stagger: 0.5,
 })
 
-gsap.to("#chili", {
+gsap.to('#chili', {
 	scrollTrigger: {
-		trigger: "#chili",
-		pin: "#chili",
-		scrub: 0.5
+		trigger: '#chili',
+		pin: '#chili',
+		scrub: 0.5,
 	},
 	scale: 3.4,
-	transformOrigin: '100% 90%'
+	transformOrigin: '100% 90%',
 })
 
 // Grafiek pagina
 gsap.set('#Balk_avocado, #Balk_sinaasappel, #Balk_Tomaat', {
 	rotation: 180,
-	transformOrigin: '50% 50%'
+	transformOrigin: '50% 50%',
 })
 
 gsap.timeline({
@@ -157,16 +155,16 @@ gsap.timeline({
 		trigger: '#watervoetafdruk',
 		pin: '#watervoetafdruk',
 		scrub: 0.5,
-		start: '5% top',
+		start: '0% top',
 		end: '+=100%',
-		pinSpacing: true
-	}
+		pinSpacing: true,
+	},
 })
 	.from('#Balk_avocado, #Balk_sinaasappel, #Balk_Tomaat', {
-		height: 0
+		height: 0,
 	})
 	.from('#tekst_balk, #tekst_balk, #Avocado_2_, #Sinaasappel_1_, #Tomaat_2_', {
-		opacity: 0
+		opacity: 0,
 	})
 
 // Rivier pagina
@@ -176,43 +174,43 @@ gsap.timeline({
 		pin: '#rivier',
 		scrub: 0.5,
 		end: '+=100%',
-		pinSpacing: true
+		pinSpacing: true,
 	},
 })
 	.to('#rivier_water', {
-		backgroundColor: 'red'
+		backgroundColor: 'red',
 	})
 	.from('#Man', {
 		x: '-600',
-		duration: 5
+		duration: 5,
 	})
 	.from('#Vrouw', {
 		x: '600',
-		duration: 5
+		duration: 5,
 	})
 	.from('#Man', {
 		x: '0',
-		duration: 5
+		duration: 5,
 	})
 	.from('#Vrouw', {
 		x: '0',
-		duration: 5
+		duration: 5,
 	})
 	.from('#droge_rivier', {
 		x: '-600',
-		duration: 5
+		duration: 5,
 	})
 	.from('#vrachtwagen', {
 		x: '600',
-		duration: 5
+		duration: 5,
 	})
 	.from('#droge_rivier', {
 		x: '0',
-		duration: 5
+		duration: 5,
 	})
 	.from('#vrachtwagen', {
 		x: '0',
-		duration: 5
+		duration: 5,
 	})
 
 gsap.timeline({
@@ -221,28 +219,28 @@ gsap.timeline({
 		pin: '#rivier',
 		scrub: 0.5,
 		end: '+=100%',
-		pinSpacing: true
+		pinSpacing: true,
 	},
 })
 	.to('#Streepje1, #Streepje2', {
 		y: '2vw',
-		fill: '#84786A'
+		fill: '#84786A',
 	})
 	.to('#Streepje4, #Streepje5', {
 		x: '-2vw',
-		fill: '#84786A'
+		fill: '#84786A',
 	})
 	.to('#Streepje6', {
 		x: '2vw',
 		y: '3.5vw',
 		fill: '#84786A',
-		rotation: -20
+		rotation: -20,
 	})
 	.to('#Streepje3', {
 		x: '3vw',
 		y: '2vw',
 		fill: '#84786A',
-		rotation: 25
+		rotation: 25,
 	})
 
 gsap.to('#rivier_water', {
@@ -251,9 +249,9 @@ gsap.to('#rivier_water', {
 		pin: '#rivier',
 		scrub: 0.5,
 		end: '+=100%',
-		pinSpacing: true
+		pinSpacing: true,
 	},
-	fill: '#A59589'
+	fill: '#A59589',
 })
 
 // Wereldkaart pagina
@@ -264,23 +262,23 @@ gsap.timeline({
 		scrub: 0.5,
 		start: 'top top',
 		end: '+=100%',
-		pinSpacing: true
+		pinSpacing: true,
 	},
 })
 	.from('#wereldkaart', {
 		scale: 3.5,
 		transformOrigin: '22% 98%',
-		duration: 8
+		duration: 8,
 	})
 	.from('#Streepjes>*', {
 		opacity: 0,
-		stagger: 0.5
+		stagger: 0.5,
 	})
 	.to('#Streepjes', {
-		opacity: 0
+		opacity: 0,
 	})
 	.to('#wereldkaart', {
 		scale: 6,
 		duration: 8,
-		transformOrigin: '51% 38%'
+		transformOrigin: '51% 38%',
 	})
